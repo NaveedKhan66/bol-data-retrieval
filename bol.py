ae52437b51d357f93ec991ef328f357c17b314a9
Naveed Khan <its.naveedkhan66@gmail.com>
2024-04-14 14:32:53 +0500

    Update: Update the schema to add client_id in the tables

--- bol.py
+++ bol.py
@@ -97,17 +97,20 @@ def is_within_threshold_percent(threshold, price, value):
 def format_offer_data(offer, row):
     return {
         "price": offer["price"],
-        "job_id": row["batch_id"],
-        "bol_offer_id": offer["offerId"],
-        "retailer_id": offer["retailerId"],
-        "country_code": offer["countryCode"],
-        "best_offer": offer["bestOffer"],
-        "price": offer["price"],
-        "fulfilment_method": offer["fulfilmentMethod"],
-        "condition": offer["condition"],
-        "ultimate_order_time": offer["ultimateOrderTime"],
-        "min_delivery_date": offer["minDeliveryDate"],
-        "max_delivery_date": offer["maxDeliveryDate"],
+        "job_id": row.get("batch_id"),
+        "EAN": row.get("EAN"),
+        "user_retailer_id": row.get("retailer_id"),
+        "client_id": row.get("client_id"),
+        "bol_offer_id": offer.get("offerId"),
+        "retailer_id": offer.get("retailerId"),
+        "country_code": offer.get("countryCode"),
+        "best_offer": offer.get("bestOffer"),
+        "price": offer.get("price"),
+        "fulfilment_method": offer.get("fulfilmentMethod"),
+        "condition": offer.get("condition"),
+        "ultimate_order_time": offer.get("ultimateOrderTime"),
+        "min_delivery_date": offer.get("minDeliveryDate"),
+        "max_delivery_date": offer.get("maxDeliveryDate"),
     }
 
 
@@ -132,7 +135,7 @@ def fetch_product_offers(jwt_token, product_data, row, page=1):
 
         response_data = response.json()
         process_offers(response_data, row, product_data)
-        # logging.info("This is response_data", response_data)
+        logging.info(f"This is response_data: {response_data}")
         if len(response_data.get("offers", [])) == 50:
             fetch_product_offers(jwt_token, row, product_data, page + 1)
 
@@ -154,12 +157,11 @@ def fetch_data_worker(request):
     for row in query_job:
 
         jwt_token = Authorization_middleware(jwt_token)
-        # logging.info("request_id=======>: ", row["request_id"])
+        request_id = row["request_id"]
+        logging.info(f"request_id=======>: {request_id} ")
         product_data = []
         fetch_product_offers(jwt_token, product_data, row)
         processed_row = prepare_row_for_insertion(row)
-
-        # logging.info(processed_row, "product_data")
 
         insert_into_bigquery(client, BOL_EAN_DATA.get("table_id"), [processed_row])
         if product_data:
@@ -183,5 +185,4 @@ def prepare_row_for_insertion(row):
 def insert_into_bigquery(client, table_id, data):
     errors = client.insert_rows_json(table_id, data)
     if errors:
-        pass
-        # logging.info(f"Errors occurred while inserting into {table_id}:", errors)
+        logging.info(f"Errors occurred while inserting into {table_id}: {errors}")
